815eb77966c31105e6b6801e127e6f736a4741fa
Mahesh-Motale77 <motalemahesh77@gmail.com>
2024-03-25 01:11:44 +0530

    Adding Lowecase and Preview , count functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,6 +6,11 @@ export default function TextForm() {
         setText(newText);
     }
 
+    const handleLowClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
     const handleOnChange = (event) => {
         setText(event.target.value)
     }
@@ -16,7 +21,15 @@ export default function TextForm() {
             <div className="mt-5 container">
                 <h3>Enter the text to analyze below...</h3>
                 <textarea className="mb-4 form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="10"></textarea>
-                <button className="btn btn-primary" onClick={handleUpClick}> Convert to Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}> Convert to Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLowClick}> Convert to Lowercase</button>
+            </div>
+            <div className="mt-2 container">
+                <h4>Your Text Summary ...</h4>
+                <p> {text.split(" ").length} Words and {text.length} Characters </p>
+                <p> {0.008 * text.split(" ").length} Minutes read </p>
+                <h4>Preview: </h4>
+                <p> {text} </p>
             </div>
         </>
     )
