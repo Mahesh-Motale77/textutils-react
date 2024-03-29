c475497446329ae05de83c326c92306982770fa0
Mahesh-Motale77 <motalemahesh77@gmail.com>
2024-03-29 21:00:50 +0530

    Dark Mode functionality added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, {useState} from 'react'
+
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
     <>
-      <Navbar/>
-      <TextForm/>
+      <Navbar mode={mode} toggleMode={toggleMode}/>
+      <TextForm mode={mode}/>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 
-export default function TextForm() {
+export default function TextForm(props) {
     const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
@@ -34,16 +34,16 @@ export default function TextForm() {
     const [text, setText] = useState("Enter text here ... ")
     return (
         <>
-            <div className="mt-5 container">
+            <div className="mt-5 container" style={{color: props.mode==='light'?'#042743':'white'}}>
                 <h3>Enter the text to analyze below...</h3>
-                <textarea className="mb-4 form-control" value={text} onChange={handleOnChange} id="myTextArea" rows="10"></textarea>
+                <textarea className="mb-4 form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='light'?'white':'gray', color: props.mode==='light'?'#042743':'white'}} id="myTextArea" rows="10"></textarea>
                 <button className="btn btn-primary mx-2" onClick={handleUpClick}> Convert to Uppercase</button>
                 <button className="btn btn-primary mx-2" onClick={handleLowClick}> Convert to Lowercase</button>
                 <button className="btn btn-primary mx-2" onClick={handleClearClick}> Clear Text</button>
                 <button className="btn btn-primary mx-2" onClick={handleCopy}> Copy Text</button>
                 <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
             </div>
-            <div className="mt-2 container">
+            <div className="mt-2 container" style={{color: props.mode==='light'?'#042743':'white'}}>
                 <h4>Your Text Summary ...</h4>
                 <p> {text.split(" ").length} Words and {text.length} Characters </p>
                 <p> {0.008 * text.split(" ").length} Minutes read </p>
